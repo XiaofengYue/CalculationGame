709c87161a661d7afc1a7f95a339abca10d09cc3
XiaoFeng <736659711@qq.com>
2018-07-05 08:50:17 +0800

    11

---
+++ CalculationGGGame/CalculationGGGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculationGGGame
+//
+//  Created by YXF on 2018/7/5.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculationGGGame/CalculationGGGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CalculationGGGame
+//
+//  Created by YXF on 2018/7/5.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
